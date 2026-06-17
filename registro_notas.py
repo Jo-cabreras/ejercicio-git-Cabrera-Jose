6c0bc8fb9562288535dada4683295ac3de1eb50e
AV-Alumno <AV-Alumno@duoc.acad>
2026-06-17 19:34:27 -0400

    Agrega validacion de aprobacion

--- registro_notas.py
+++ registro_notas.py
@@ -3,3 +3,8 @@ nombre = input("Ingrese nombre del estudiante: ")
 nota = float(input("Ingrese nota final: "))
 print("Estudiante:", nombre)
 print("Nota final:", nota)
+
+if nota>4.0:
+    print(f"El estudiante aprobo con un {nota}")
+else:
+    print(f"El estudiante desaprobo con un {nota}")
